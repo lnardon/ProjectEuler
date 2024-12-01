e49a6d9d644d776c20b80d3eab8dcf99e06b6d9c
Lucas Nardon <43593024+lnardon@users.noreply.github.com>
2024-12-01 06:54:57 +0000

    Finish algo

---
+++ SpecialPythagoreanTriplet/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+// A Pythagorean triplet is a set of three natural numbers, $a \lt b \lt c$, for which,
+// a^2 + b^2 = c^2.$$
+// For example, $3^2 + 4^2 = 9 + 16 = 25 = 5^2$.
+// There exists exactly one Pythagorean triplet for which $a + b + c = 1000$.<br>Find the product $abc$.
+func main() {
+	for a := 1; a < 1000; a++ {
+		for b := a + 1; b < 1000; b++ {
+			c := 1000 - a - b
+
+			if a*a+b*b == c*c {
+				fmt.Printf("Product: %d\n", a*b*c)
+				return
+			}
+		}
+	}
+}
